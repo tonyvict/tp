3b827267a8a4a23701b2cea54a7cb18a89aa9a5c
Pranav <vanarp2205@gmail.com>
2025-10-09 01:33:18 +0800

    Remove executeTest

--- src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -23,13 +23,6 @@ public class RemarkCommandTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
 
-    @Test
-    public void execute() {
-        final String remark = "Some remark";
-
-        assertCommandFailure(new RemarkCommand(INDEX_FIRST_PERSON, remark), model,
-                String.format(MESSAGE_ARGUMENTS, INDEX_FIRST_PERSON.getOneBased(), remark));
-    }
 
     @Test
     public void equals() {
