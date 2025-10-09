2eb1a527f764929be4ac39292ba32260c1f442b0
tony_vict <rtonyvictar@gmail.com>
2025-10-09 11:38:22 +0800

    Fix test consistency: Update TypicalPersons.java to match JSON data

--- src/test/java/seedu/address/testutil/TypicalPersons.java
+++ src/test/java/seedu/address/testutil/TypicalPersons.java
@@ -23,7 +23,7 @@ import seedu.address.model.person.Person;
  */
 public class TypicalPersons {
 
-    public static final Person ALICE = new PersonBuilder().withName("Alice Pauline")
+    public static final Person ALICE = new PersonBuilder().withName("Alice Paul")
             .withAddress("123, Jurong West Ave 6, #08-111").withEmail("alice@example.com")
             .withPhone("94351253")
             .withTags("friends").build();
