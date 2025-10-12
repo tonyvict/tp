50cf1a89adabf3c67d69c2b06d44001939e75de6
Jaycee <jiacheng2003@gmail.com>
2025-10-12 23:09:04 +0800

    add message usage

--- src/main/java/seedu/address/logic/commands/ScheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/ScheduleCommand.java
@@ -9,6 +9,17 @@ public class ScheduleCommand extends Command {
 
     public static final String COMMAND_WORD = "schedule";
 
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Edits the lesson of the person identified "
+            + "by the index number used in the last person listing. "
+            + "Existing lesson will be overwritten by the input.\n"
+            + "Parameters: INDEX (must be a positive integer) "
+            + "start/ [START TIME] end/ [END TIME} "
+            + "date/ [DATE] sub/ [SUBJECT]\n"
+            + "Example: " + COMMAND_WORD + " 1 "
+            + "start/ 09:30 end/ 11:30 "
+            + "date/ 2025-09-20 sub/ Maths";
+
     @Override
     public CommandResult execute(Model model) {
         return new CommandResult("Hello from schedule");
