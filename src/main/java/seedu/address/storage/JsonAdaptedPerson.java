61e2fea6e31dc8ab09f79c9179949875bcc5d6d6
Jaycee <jiacheng2003@gmail.com>
2025-10-13 16:12:55 +0800

    Update Storage

--- src/main/java/seedu/address/storage/JsonAdaptedPerson.java
+++ src/main/java/seedu/address/storage/JsonAdaptedPerson.java
@@ -10,14 +10,8 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.person.Remark;
+import seedu.address.model.person.*;
 import seedu.address.model.tag.Tag;
-import seedu.address.model.person.LessonList;
 
 /**
  * Jackson-friendly version of {@link Person}.
@@ -32,6 +26,7 @@ class JsonAdaptedPerson {
     private final String address;
     private final String remark;
     private final List<JsonAdaptedTag> tags = new ArrayList<>();
+    private final List<JsonAdaptedLesson> lessonList = new ArrayList<>();
 
     /**
      * Constructs a {@code JsonAdaptedPerson} with the given person details.
@@ -39,7 +34,8 @@ class JsonAdaptedPerson {
     @JsonCreator
     public JsonAdaptedPerson(@JsonProperty("name") String name, @JsonProperty("phone") String phone,
                              @JsonProperty("email") String email, @JsonProperty("address") String address,
-                             @JsonProperty("remark") String remark, @JsonProperty("tags") List<JsonAdaptedTag> tags) {
+                             @JsonProperty("remark") String remark, @JsonProperty("tags") List<JsonAdaptedTag> tags,
+                             @JsonProperty("lessonList") List<JsonAdaptedLesson> lessonList) {
         this.name = name;
         this.phone = phone;
         this.email = email;
@@ -47,6 +43,9 @@ class JsonAdaptedPerson {
         this.remark = remark;
         if (tags != null) {
             this.tags.addAll(tags);
+        }
+        if (lessonList != null) {
+            this.lessonList.addAll(lessonList);
         }
     }
 
@@ -62,6 +61,9 @@ class JsonAdaptedPerson {
         tags.addAll(source.getTags().stream()
                 .map(JsonAdaptedTag::new)
                 .collect(Collectors.toList()));
+        lessonList.addAll(source.getLessonList().getLessons().stream()
+                .map(JsonAdaptedLesson::new)
+                .collect(Collectors.toList()));
     }
 
     /**
@@ -73,6 +75,11 @@ class JsonAdaptedPerson {
         final List<Tag> personTags = new ArrayList<>();
         for (JsonAdaptedTag tag : tags) {
             personTags.add(tag.toModelType());
+        }
+
+        final ArrayList<Lesson> personLessons = new ArrayList<>();
+        for (JsonAdaptedLesson lesson : lessonList) {
+            personLessons.add(lesson.toModelType());
         }
 
         if (name == null) {
@@ -110,8 +117,9 @@ class JsonAdaptedPerson {
         if (remark == null) {
             throw new IllegalValueException(String.format(MISSING_FIELD_MESSAGE_FORMAT, Remark.class.getSimpleName()));
         }
+
         final Remark modelRemark = new Remark(remark);
-        final LessonList modelLessonList = new LessonList(); //TODO: Implement parsing and marshalling in the storage commit
+        final LessonList modelLessonList = new LessonList(personLessons);
 
         final Set<Tag> modelTags = new HashSet<>(personTags);
         return new Person(modelName, modelPhone, modelEmail, modelAddress, modelRemark, modelTags, modelLessonList);
