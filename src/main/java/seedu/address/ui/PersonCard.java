c2d6becf2cca39aae5e2e148987e4e6e8a420c36
Samuel <samueltanwj@gmail.com>
2025-10-15 10:55:26 +0800

    Update UI to show attributes of contact

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
+import seedu.address.model.person.Attribute;
 import seedu.address.model.person.Person;
 
 /**
@@ -42,6 +43,8 @@ public class PersonCard extends UiPart<Region> {
     private FlowPane tags;
     @FXML
     private Label remark;
+    @FXML
+    private FlowPane attributes;
 
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
@@ -58,5 +61,11 @@ public class PersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
+        person.getAttributes().stream()
+                .sorted(Comparator.comparing(Attribute::getKey))
+                .forEach(attr -> {
+                    String displayText = attr.getKey() + ": " + String.join(", ", attr.getValues() + " |");
+                    attributes.getChildren().add(new Label(displayText));
+                });
     }
 }
