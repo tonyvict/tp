860fca6580f0ebc68c9522a94b550dc2738cfd92
Jaycee <jiacheng2003@gmail.com>
2025-10-22 18:41:41 +0800

    Add assertions and logging

--- src/main/java/seedu/address/logic/commands/UnscheduleCommand.java
+++ src/main/java/seedu/address/logic/commands/UnscheduleCommand.java
@@ -5,6 +5,9 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.util.List;
 
+import java.util.logging.Logger;
+
+import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
@@ -30,6 +33,8 @@ public class UnscheduleCommand extends Command {
     public static final String MESSAGE_REMOVE_LESSON_SUCCESS = "Removed lesson %1$s from Person: %2$s";
     public static final String MESSAGE_NO_LESSONS = "The selected person has no lessons scheduled.";
 
+    private static final Logger logger = LogsCenter.getLogger(UnscheduleCommand.class);
+
     private final Index personIndex;
     private final Index lessonIndex;
 
@@ -45,26 +50,35 @@ public class UnscheduleCommand extends Command {
 
     @Override
     public CommandResult execute(Model model) throws CommandException {
-        requireAllNonNull(model);
+        requireAllNonNull(model, personIndex, lessonIndex);
+        logger.fine(() -> String.format("Executing UnscheduleCommand for person index %s, lesson index %s",
+                personIndex.getOneBased(), lessonIndex.getOneBased()));
+
         List<Person> lastShownList = model.getFilteredPersonList();
 
         if (personIndex.getZeroBased() >= lastShownList.size()) {
+            logger.fine("UnscheduleCommand failed due to invalid person index.");
             throw new CommandException(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
         }
 
         Person personToEdit = lastShownList.get(personIndex.getZeroBased());
+        assert personToEdit != null : "Person to edit should not be null";
         LessonList lessonList = personToEdit.getLessonList();
 
         if (lessonList.isEmpty()) {
+            logger.fine("UnscheduleCommand found no lessons to remove.");
             throw new CommandException(MESSAGE_NO_LESSONS);
         }
 
         if (lessonIndex.getZeroBased() >= lessonList.size()) {
+            logger.fine("UnscheduleCommand failed due to invalid lesson index.");
             throw new CommandException(Messages.MESSAGE_INVALID_LESSON_DISPLAYED_INDEX);
         }
 
         Lesson lessonToRemove = lessonList.get(lessonIndex.getZeroBased());
         LessonList updatedLessonList = lessonList.remove(lessonToRemove);
+        assert updatedLessonList.size() == lessonList.size() - 1
+                : "Lesson list should decrease by exactly one lesson";
 
         Person editedPerson = new Person(
                 personToEdit.getName(), personToEdit.getPhone(), personToEdit.getEmail(),
@@ -73,6 +87,9 @@ public class UnscheduleCommand extends Command {
 
         model.setPerson(personToEdit, editedPerson);
         model.updateFilteredPersonList(PREDICATE_SHOW_ALL_PERSONS);
+
+        logger.fine(() -> String.format("UnscheduleCommand removed lesson %s from person %s",
+                lessonIndex.getOneBased(), personIndex.getOneBased()));
 
         return new CommandResult(String.format(MESSAGE_REMOVE_LESSON_SUCCESS,
                 lessonToRemove, Messages.format(editedPerson)));
