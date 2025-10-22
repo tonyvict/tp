2aa9ae140f519d9639275c60784e3560db13d6a6
tony_vict <rtonyvictar@gmail.com>
2025-10-22 12:39:26 +0800

    docs: improve Person class documentation
    - Add note about grades, attributes, and lessons support
    - Clarify enhanced functionality in class comment

--- src/main/java/seedu/address/model/person/Person.java
+++ src/main/java/seedu/address/model/person/Person.java
@@ -13,6 +13,8 @@ import seedu.address.model.tag.Tag;
 /**
  * Represents a Person in the address book.
  * Guarantees: details are present and not null, field values are validated, immutable.
+ * 
+ * This class now includes support for grades, attributes, and lessons.
  */
 public class Person {
 
