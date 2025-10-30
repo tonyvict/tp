792e262e5244bffe39047be05a4a6982cf88548a
Jaycee <jiacheng2003@gmail.com>
2025-10-30 15:15:20 +0800

    Change nothing

--- src/main/java/seedu/address/logic/parser/ScheduleCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ScheduleCommandParser.java
@@ -2,10 +2,8 @@ package seedu.address.logic.parser;
 
 import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_END;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_START;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_SUB;
+import static seedu.address.logic.parser.CliSyntax.*;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
