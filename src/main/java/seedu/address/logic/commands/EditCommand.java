56c4d61713778760eadcf6347b7d2c6b6f4764be
Pranav <vanarp2205@gmail.com>
2025-11-02 23:28:40 +0800

    fix style

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -23,7 +23,6 @@ import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.person.Address;
 import seedu.address.model.person.Attribute;
-import seedu.address.model.person.Attribute;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.LessonList;
 import seedu.address.model.person.Name;
